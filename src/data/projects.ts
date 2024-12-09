fa9ebe784ed59f2e20483416c45768fcfab4bb92
Ivan200499 <zampadue@gmail.com>
2024-12-09 21:03:56 +0100

    Fix: Update image paths and add images to public folder

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const projectsData = [
     id: 1,
     title: 'Gestionale per Ristorazione con Ordini Digitalizzati e QR Code',
     category: 'Sviluppo Software',
-    image: '/ristorazione.PNG',
+    image: '/images/ristorazione.PNG',
     description: 'Ottimizza il flusso di lavoro e migliora l\'esperienza del cliente con ordini digitalizzati.',
     technology: 'React, Node.js, QR Code',
     client: 'Ristorante Innovativo',
@@ -12,15 +12,15 @@ export const projectsData = [
     solution: 'Abbiamo sviluppato un gestionale che utilizza QR code per digitalizzare il processo di ordinazione, migliorando l\'efficienza operativa.',
     result: 'Riduzione dei tempi di attesa del 30% e miglioramento della soddisfazione del cliente.',
     gallery: [
-      '/ristorazione.PNG',
-      '/ristorazione.PNG'
+      '/images/ristorazione.PNG',
+      '/images/ristorazione.PNG'
     ]
   },
   {
     id: 2,
     title: 'Menu Digitale per Bar Gorah',
     category: 'Web Design',
-    image: '/Cattura2.PNG',
+    image: '/images/Cattura2.PNG',
     description: 'Un semplice menu digitale per migliorare l\'esperienza del cliente.',
     technology: 'React, TailwindCSS',
     client: 'Bar Gorah',
@@ -29,9 +29,8 @@ export const projectsData = [
     solution: 'Abbiamo creato un menu digitale intuitivo e facile da navigare, accessibile tramite QR code.',
     result: 'Aumento del 20% nell\'efficienza del servizio e miglioramento dell\'esperienza del cliente.',
     gallery: [
-      '/Cattura2.PNG',
-      '/Cattura2.PNG'
+      '/images/Cattura2.PNG',
+      '/images/Cattura2.PNG'
     ]
-  },
- 
+  }
 ];
