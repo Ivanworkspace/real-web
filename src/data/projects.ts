7141e08bb978e1ab2b8ac32225fa005788ad1e15
Ivan200499 <zampadue@gmail.com>
2025-01-30 16:31:44 +0100

    Rimuovi l'anteprima dei progetti dalla homepage e aggiungi nuovi progetti

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,8 +6,8 @@ export const projectsData = [
     image: '/images/ristorazione.PNG',
     description: 'Ottimizza il flusso di lavoro e migliora l\'esperienza del cliente con ordini digitalizzati.',
     technology: 'React, Node.js, QR Code',
-    client: 'Ristorante Innovativo',
-    date: '2023',
+    client: 'Attività che richiedono un sistema per gestire gli ordini',
+    date: '2024',
     challenge: 'Il cliente necessitava di un sistema per gestire gli ordini in modo più efficiente e ridurre i tempi di attesa.',
     solution: 'Abbiamo sviluppato un gestionale che utilizza QR code per digitalizzare il processo di ordinazione, migliorando l\'efficienza operativa.',
     result: 'Riduzione dei tempi di attesa del 30% e miglioramento della soddisfazione del cliente.',
@@ -24,13 +24,47 @@ export const projectsData = [
     description: 'Un semplice menu digitale per migliorare l\'esperienza del cliente.',
     technology: 'React, TailwindCSS',
     client: 'Bar Gorah',
-    date: '2023',
+    date: '2024',
     challenge: 'Il bar necessitava di un menu digitale per facilitare l\'accesso alle informazioni sui prodotti.',
     solution: 'Abbiamo creato un menu digitale intuitivo e facile da navigare, accessibile tramite QR code.',
     result: 'Aumento del 20% nell\'efficienza del servizio e miglioramento dell\'esperienza del cliente.',
     gallery: [
       '/images/Cattura2.PNG',
       '/images/Cattura2.PNG'
+    ]
+  },
+  {
+    id: 3,
+    title: 'Luxury Jewel Accesories',
+    category: 'Brand di Gioielli',
+    image: '/images/nuovo_progetto1.jpg',
+    description: 'Descrizione del nuovo progetto 1.',
+    technology: 'React, TailwindCSS',
+    client: 'Chiara Martone',
+    date: '2025',
+    challenge: 'Creare un brand di gioielli di lusso',
+    solution: 'Grazie alla mia esperienza nel settore, ho realizzato un servizio fotografico professionale per valorizzare al meglio i gioielli, creando contenuti di alta qualità. Abbiamo sviluppato una strategia di marketing organica sui social media, puntando su storytelling visivo ed engagement per costruire una connessione autentica con il pubblico.',
+    result: 'Abbiamo aumentato la brand awareness e fidelizzato i clienti attraverso contenuti curati e una community coinvolta, consolidando la presenza del brand nel settore del lusso.',
+    gallery: [
+      '/images/modella1.jpg',
+      '/images/modella2.jpg'
+    ]
+  },
+  {
+    id: 4,
+    title: 'Portfolio Valentina Dell\'Aquila',
+    category: 'Sito Web',
+    image: '/images/valentina.png', //TODO: cambiare immagine
+    description: 'Descrizione del nuovo progetto 2.',
+    technology: 'React, TailwindCSS',
+    client: 'Valentina Dell\'Aquila',
+    date: '2025',
+    challenge: 'Creare un sito web professionale per presentare una modella e il suo portfolio in modo elegante e coinvolgente..',
+    solution: 'Abbiamo sviluppato un sito web moderno e minimalista, con un layout pulito che valorizza le immagini e i contenuti. Il design responsive garantisce un/ottima esperienza su tutti i dispositivi, mentre la navigazione intuitiva permette di esplorare facilmente il portfolio',
+    result: 'Il sito ha rafforzato il personal branding della modella, aumentando le opportunità di collaborazione con fotografi e brand. Grazie alla struttura visiva accattivante, ha ottenuto maggiore visibilità e coinvolgimento sui social media.',
+    gallery: [
+      '/images/foto-vale.png',
+      '/images/foto-vale2.png'
     ]
   }
 ];
